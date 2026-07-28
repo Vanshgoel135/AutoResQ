3e6311e5d8f80e1b005f6cf24bb6a196ee0b2598
Vansh Goel <goelvansh71@gmail.com>
2026-07-28 15:41:36 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/autoresq/AutoresqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autoresq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoresqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoresqBackendApplication.class, args);
+	}
+
+}
