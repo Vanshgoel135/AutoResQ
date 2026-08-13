1c8c02d6a699854f233f701320b6fc2530db5e98
Vansh Goel <goelvansh71@gmail.com>
2026-08-13 23:33:40 +0530

    normaltext password conver into  BCrypt then save in Database

--- src/main/java/com/autoresq/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/autoresq/user/service/impl/UserServiceImpl.java
@@ -1,24 +1,37 @@
 package com.autoresq.user.service.impl;
-
+import com.autoresq.user.service.UserService;
 import com.autoresq.user.dto.RegisterRequest;
-import org.springframework.stereotype.Service;
+import com.autoresq.user.entity.User;
 import com.autoresq.user.repository.UserRepository;
 import com.autoresq.user.service.UserService;
-import com.autoresq.user.entity.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 @Service
 public class UserServiceImpl implements UserService {
-    public final UserRepository userRepository;
-    public UserServiceImpl(UserRepository userRepository) {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository,
+                           PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
+
     @Override
-   public void register(RegisterRequest request){
-        User user=new User();
+    public void register(RegisterRequest request) {
+
+        User user = new User();
+
         user.setFirstName(request.getFirstName());
         user.setLastName(request.getLastName());
         user.setEmail(request.getEmail());
         user.setPhone(request.getPhone());
-        user.setPassword(request.getPassword());
+
+        // Password ko BCrypt se encode karke save karna
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
         user.setRole("USER");
 
         userRepository.save(user);
