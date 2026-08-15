3a32e8ede4bc702f33051e39a85443bcad5d9230
Vansh Goel <goelvansh71@gmail.com>
2026-08-16 01:11:53 +0530

    jwt token

--- src/main/java/com/autoresq/security/JwtAuthenticationFilter.java
+++ src/main/java/com/autoresq/security/JwtAuthenticationFilter.java
@@ -1,4 +1,23 @@
 package com.autoresq.security;
+package com.autoresq.security;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        filterChain.doFilter(request, response);
+    }
 }
